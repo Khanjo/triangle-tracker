30f1d6ce7852fe5ce6557576fabae61f3a53778b
Joshua Khan <Khanjo@gmail.com>
2023-06-01 15:58:10 -0700

    initialize project

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+window.addEventListener("load", function () {
+    let form = document.querySelector("form");
+
+    form.addEventListener("submit", function (event) {
+        const sideAInput = document.getElementById("sideAInput");
+        const sideBInput = document.getElementById("sideBInput");
+        const sideCInput = document.getElementById("sideCInput");
+
+
+    })
+});
