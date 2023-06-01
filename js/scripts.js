87b8564fc3d8d7e01f5a0f8d694e5ec27979fe5e
Joshua Khan <Khanjo@gmail.com>
2023-06-01 16:22:46 -0700

    identify equilateral triangles

--- js/scripts.js
+++ js/scripts.js
@@ -2,10 +2,14 @@ window.addEventListener("load", function () {
     let form = document.querySelector("form");
 
     form.addEventListener("submit", function (event) {
-        const sideAInput = document.getElementById("sideAInput");
-        const sideBInput = document.getElementById("sideBInput");
-        const sideCInput = document.getElementById("sideCInput");
+        const A = document.getElementById("sideAInput").value;
+        const B = document.getElementById("sideBInput").value;
+        const C = document.getElementById("sideCInput").value;
 
+        if (A === B, A === C) {
+            document.querySelector("span#result").innerText = " Equilateral";
+        }
 
-    })
+        event.preventDefault();
+    });
 });
